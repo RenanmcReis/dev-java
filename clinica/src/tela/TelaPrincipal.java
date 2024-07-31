fba8914d5610ac0c5acd940591e17250f1c2ceeb
renan <renan@Valquiria>
2024-07-31 16:12:23 -0300

    primeiro commit

---
+++ clinica/src/tela/TelaPrincipal.java
@@ -0,0 +1,32 @@
+package tela;
+/**
+ * essa clase representa... JAVADOC
+ * @author Renan Reis <renanmc.reis@gmail.com>
+ * @since JDK1.0
+ * 
+ * 
+ */
+public class TelaPrincipal {
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	//tudo começa a partir de um main!!
+	public static void main(String[] args) {
+		/*
+		 * 
+		 * 
+		 * 
+		 */
+		System.out.println("teste");
+	}
+}
+
+
+
+
+
+
+
+
