a56e525d5713f6532cd5e11b216aa9063874f60a
renan <renan@Valquiria>
2024-07-31 16:25:10 -0300

    Executanto commit teste

--- clinica/src/administracao/Agenda.java
+++ clinica/src/administracao/Agenda.java
@@ -4,5 +4,4 @@ public class Agenda {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 	}
-
 }
--- clinica/src/tela/TelaAgendamento.java
+++ clinica/src/tela/TelaAgendamento.java
@@ -1,5 +1,8 @@
 package tela;
 
 public class TelaAgendamento {
+	public static void main(String[] args) {
+		System.out.println("tela com informacoes !!!");
+	}
 
 }
